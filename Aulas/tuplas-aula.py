3ef9c4f2a823d8bf7fb22ebcbd06bb6f59c82a6d
Renata Santos <renatados.santosp@gmail.com>
2021-11-14 14:43:32 -0300

    Update tuplas-aula.py

--- Aulas/tuplas-aula.py
+++ Aulas/tuplas-aula.py
@@ -4,7 +4,7 @@
 
 # são construídas com o uso de parênteses () e vírgulas separando cada elemento da tupla
 
-tupla = (item1, item2,..., itemn)
+tupla = ('item1', 'item2', 'item3')
 
 # se no programa vc precisa ter CERTEZA de que os dados NÃO SOFRERÃO mudanças, então tuplas podem ser a solução
 
@@ -13,3 +13,16 @@ tupla = (item1, item2,..., itemn)
 #listas = []
 #dicionários = {}
 #tuplas = ()
+
+#usando a função list() para converter uma tupla para lista
+t2 = ('A', 'B', 'C')
+lista_tupla = list(t2)
+print(lista_tupla)
+
+#vamos colocar agr um item a mais na lista
+lista_tupla.append('D')
+print(lista_tupla)
+
+#agora reconvertendo...
+t2 = tuple(lista_tupla)
+print(t2)
