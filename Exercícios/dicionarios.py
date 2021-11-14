42b1331c08a2eeff527a9cf7e1fc75f9c70c79a8
Renata Santos <renatados.santosp@gmail.com>
2021-11-14 14:23:30 -0300

    Create dicionarios.py

---
+++ Exercícios/dicionarios.py
@@ -0,0 +1,8 @@
+estudantes = ["Matheus", 24, "Fernanda", 23]
+estudantes_dic = {'Matheus': 24, 'Fernanda': 23}
+estudantes_dic['Pedro'] = 23
+print(estudantes_dic)
+print(len(estudantes_dic))
+print("As chaves do dicionário são: ", estudantes_dic.keys())
+print("Os valores no dicionário são: ", estudantes_dic.values())
+print("Os itens do dicionário são: ", estudantes_dic.items())
